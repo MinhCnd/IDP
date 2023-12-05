221bf7ed9c0bcc01d31c388f3a37e26cfaa16f08
MinhCao <minh.cnd@gmail.com>
2023-12-05 21:05:06 +1300

    fix: use isclose instead of comparing float directly

--- idp/main.py
+++ idp/main.py
@@ -2,6 +2,7 @@ from transformers import AutoModelForTokenClassification
 from fastapi import FastAPI, UploadFile, HTTPException
 from pdf2image import convert_from_bytes
 from transformers import AutoProcessor
+from math import isclose
 
 import time
 
@@ -70,6 +71,8 @@ def clean_model_output(output: Dict[str, any]) -> Dict[str, any]:
     try:
         class_to_label = {v: k for k, v in LABEL_STR_TO_CLASS_MAP.items()}
         cleaned_output = {}
+
+        # Text to numbers
         for key in list(
             filter(
                 lambda key: key
@@ -143,209 +146,208 @@ def validate_model_output(output: Dict[str, Union[float, str, None]]) -> bool:
         + output[class_to_label[Classes.WASTEWATER_FIXED]]
     )
 
-    balance_current_charges_correct = (
-        balance_current_charges
-        == output[class_to_label[Classes.BALANCE_CURRENT_CHARGES]]
-    )
+    balance_current_charges_correct = isclose(balance_current_charges,
+                                              output[class_to_label[Classes.BALANCE_CURRENT_CHARGES]])
 
     total_due = (
         output[class_to_label[Classes.BALANCE_STILL_OWING]] + balance_current_charges
     )
 
-    total_due_correct = total_due == output[class_to_label[Classes.TOTAL_DUE]]
+    total_due_correct = isclose(total_due,
+                                output[class_to_label[Classes.TOTAL_DUE]])
 
     return balance_current_charges_correct and total_due_correct
 
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
-    try:
-        start_time = time.time()
-        curr_time = time.time()
-        contents = file.file.read()
-        images = convert_from_bytes(contents)
 
-        if PRINT_DEBUG:
-            print(f"DEBUG: PDF to bytes - {time.time()-curr_time:.2} sec")
-        curr_time = time.time()
+    start_time = time.time()
+    curr_time = time.time()
+    contents = file.file.read()
+    images = convert_from_bytes(contents)
 
-        # Extract texts using OCR
-        pages = len(images)
-        text_box_pairs = [get_text_box_pairs(image) for image in images]
-        texts = [pair[0] for pair in text_box_pairs]
-        boxes = [pair[1] for pair in text_box_pairs]
-        page_indexes = [[index] * len(text_arr) for index, text_arr in enumerate(texts)]
+    if PRINT_DEBUG:
+        print(f"DEBUG: PDF to bytes - {time.time()-curr_time:.2} sec")
+    curr_time = time.time()
 
-        if PRINT_DEBUG:
-            print(f"DEBUG: PDF load - {time.time() - curr_time:.2} sec")
-        curr_time = time.time()
+    # Extract texts using OCR
+    pages = len(images)
+    text_box_pairs = [get_text_box_pairs(image) for image in images]
+    texts = [pair[0] for pair in text_box_pairs]
+    boxes = [pair[1] for pair in text_box_pairs]
+    page_indexes = [[index] * len(text_arr) for index, text_arr in enumerate(texts)]
 
-        # Assume all pages have the same size
-        img_width, img_height = images[0].size
+    if PRINT_DEBUG:
+        print(text_box_pairs)
+        print(f"DEBUG: PDF load - {time.time() - curr_time:.2} sec")
+    curr_time = time.time()
 
-        def text_box_relevant(text_box_page):
-            """HACK remove texts & boxes outside of Labelling area"""
-            text, box, page = text_box_page
+    # Assume all pages have the same size
+    img_width, img_height = images[0].size
 
-            if page == 0:
-                outer_box = normalize_box(
-                    (1298, 828, 1536, 1550), img_width, img_height
-                )
-                return is_box_a_within_box_b(box, outer_box)
-            elif page == 1:
-                outer_box = normalize_box((139, 119, 827, 1173), img_width, img_height)
-                return is_box_a_within_box_b(box, outer_box)
-            else:
-                return False
+    def text_box_relevant(text_box_page):
+        """HACK remove texts & boxes outside of Labelling area"""
+        text, box, page = text_box_page
 
-        out_texts = []
-        out_boxes = []
-
-        for page in range(pages):
-            filtered_list = list(
-                filter(
-                    text_box_relevant,
-                    list(zip(texts[page], boxes[page], page_indexes[page])),
-                )
+        if page == 0:
+            outer_box = normalize_box(
+                (1298, 828, 1536, 1550), img_width, img_height
             )
-            results = [
-                [text for text, box, page in filtered_list],
-                [box for text, box, page in filtered_list],
-            ]
-            temp_texts, temp_boxes = results
-            out_texts.append(temp_texts)
-            out_boxes.append(temp_boxes)
+            return is_box_a_within_box_b(box, outer_box)
+        elif page == 1:
+            outer_box = normalize_box((139, 119, 827, 1173), img_width, img_height)
+            return is_box_a_within_box_b(box, outer_box)
+        else:
+            return False
 
-        if PRINT_DEBUG:
-            print(f"DEBUG: Trim text boxes - {time.time() - curr_time:.2} sec")
-        curr_time = time.time()
+    out_texts = []
+    out_boxes = []
 
-        # Encode input
-        encoding = processor(
-            images=images,
-            text=out_texts,
-            boxes=out_boxes,
-            truncation=True,
-            padding="max_length",
-            return_tensors="pt",
+    for page in range(pages):
+        filtered_list = list(
+            filter(
+                text_box_relevant,
+                list(zip(texts[page], boxes[page], page_indexes[page])),
+            )
         )
+        results = [
+            [text for text, box, page in filtered_list],
+            [box for text, box, page in filtered_list],
+        ]
+        temp_texts, temp_boxes = results
+        out_texts.append(temp_texts)
+        out_boxes.append(temp_boxes)
+
+    if PRINT_DEBUG:
+        print(f"DEBUG: Trim text boxes - {time.time() - curr_time:.2} sec")
+    curr_time = time.time()
+
+    # Encode input
+    encoding = processor(
+        images=images,
+        text=out_texts,
+        boxes=out_boxes,
+        truncation=True,
+        padding="max_length",
+        return_tensors="pt",
+    )
+
+    if PRINT_DEBUG:
+        print(f"DEBUG: Encode input - {(time.time() - curr_time):.2} sec")
+    curr_time = time.time()
+
+    # Perform inferece
+    with torch.no_grad():
+        outputs = model(**encoding)
+        predictions = outputs.logits.argmax(-1).tolist()
+
+        token_boxes = encoding.bbox.tolist()
+        pages_input_ids = encoding["input_ids"].tolist()
+        PAD_ID = 1
+        # First token is a special token, ignore
+        pad_indexes = [
+            page_input_ids.index(PAD_ID)
+            for page_input_ids in encoding["input_ids"].tolist()
+        ]
+        true_predictions = [
+            [
+                model.config.id2label[pred]
+                for pred in page_preds[1 : pad_indexes[page_index]]  # noqa: E203
+            ]
+            for page_index, page_preds in enumerate(predictions)
+        ]
+        true_boxes = [
+            [
+                unnormalize_box(box, img_width, img_height)
+                for box in page_token_boxes[
+                    1 : pad_indexes[page_index]  # noqa: E203
+                ]
+            ]
+            for page_index, page_token_boxes in enumerate(token_boxes)
+        ]
+        true_texts = [
+            [
+                processor.tokenizer.decode([value])
+                for index, value in enumerate(
+                    page_input_ids[1 : pad_indexes[page_index]]  # noqa: E203
+                )
+            ]
+            for page_index, page_input_ids in enumerate(pages_input_ids)
+        ]
+
+        class_to_label_str_map = {v: k for k, v in LABEL_STR_TO_CLASS_MAP.items()}
+        output = [
+            {
+                class_to_label_str_map[key]: {"text": "", "box": []}
+                for key, value in CLASS_TO_LABEL_MAP.items()
+                if key != Classes.OTHER
+            }
+            for page in range(pages)
+        ]
+        output = [
+            {
+                class_to_label_str_map[key]: {"text": ""}
+                for key, value in CLASS_TO_LABEL_MAP.items()
+                if key != Classes.OTHER
+            }
+            for page in range(pages)
+        ]
+
+        for page_indx in range(pages):
+            for key, value in CLASS_TO_LABEL_MAP.items():
+                if key == Classes.OTHER:
+                    continue
+
+                output[page_indx][class_to_label_str_map[key]]["text"] = "".join(
+                    [
+                        text
+                        for text, prediction, box in zip(
+                            true_texts[page_indx],
+                            true_predictions[page_indx],
+                            true_boxes[page_indx],
+                        )
+                        if (prediction == value and box != [0, 0, 0, 0])
+                    ]
+                )
+                output[page_indx][class_to_label_str_map[key]][
+                    "box"
+                ] = merge_box_extremes(
+                    [
+                        box
+                        for text, prediction, box in zip(
+                            true_texts[page_indx],
+                            true_predictions[page_indx],
+                            true_boxes[page_indx],
+                        )
+                        if (prediction == value and box != [0, 0, 0, 0])
+                    ]
+                )
+
+        # #trim empty outputs
+        def item_not_empty(item):
+            return len(item[1]["text"]) != 0
+
+        filtered_output = [
+            dict(filter(item_not_empty, page_output.items()))
+            for page_output in output
+        ]
 
         if PRINT_DEBUG:
-            print(f"DEBUG: Encode input - {(time.time() - curr_time):.2} sec")
+            print(filtered_output)
+            print(f"DEBUG: Inference - {time.time() - curr_time:.2} sec")
         curr_time = time.time()
 
-        # Perform inferece
-        with torch.no_grad():
-            outputs = model(**encoding)
-            predictions = outputs.logits.argmax(-1).tolist()
-
-            token_boxes = encoding.bbox.tolist()
-            pages_input_ids = encoding["input_ids"].tolist()
-            PAD_ID = 1
-            # First token is a special token, ignore
-            pad_indexes = [
-                page_input_ids.index(PAD_ID)
-                for page_input_ids in encoding["input_ids"].tolist()
-            ]
-            true_predictions = [
-                [
-                    model.config.id2label[pred]
-                    for pred in page_preds[1 : pad_indexes[page_index]]  # noqa: E203
-                ]
-                for page_index, page_preds in enumerate(predictions)
-            ]
-            true_boxes = [
-                [
-                    unnormalize_box(box, img_width, img_height)
-                    for box in page_token_boxes[
-                        1 : pad_indexes[page_index]  # noqa: E203
-                    ]
-                ]
-                for page_index, page_token_boxes in enumerate(token_boxes)
-            ]
-            true_texts = [
-                [
-                    processor.tokenizer.decode([value])
-                    for index, value in enumerate(
-                        page_input_ids[1 : pad_indexes[page_index]]  # noqa: E203
-                    )
-                ]
-                for page_index, page_input_ids in enumerate(pages_input_ids)
-            ]
-
-            class_to_label_str_map = {v: k for k, v in LABEL_STR_TO_CLASS_MAP.items()}
-            output = [
-                {
-                    class_to_label_str_map[key]: {"text": "", "box": []}
-                    for key, value in CLASS_TO_LABEL_MAP.items()
-                    if key != Classes.OTHER
-                }
-                for page in range(pages)
-            ]
-            output = [
-                {
-                    class_to_label_str_map[key]: {"text": ""}
-                    for key, value in CLASS_TO_LABEL_MAP.items()
-                    if key != Classes.OTHER
-                }
-                for page in range(pages)
-            ]
-
-            for page_indx in range(pages):
-                for key, value in CLASS_TO_LABEL_MAP.items():
-                    if key == Classes.OTHER:
-                        continue
-
-                    output[page_indx][class_to_label_str_map[key]]["text"] = "".join(
-                        [
-                            text
-                            for text, prediction, box in zip(
-                                true_texts[page_indx],
-                                true_predictions[page_indx],
-                                true_boxes[page_indx],
-                            )
-                            if (prediction == value and box != [0, 0, 0, 0])
-                        ]
-                    )
-                    output[page_indx][class_to_label_str_map[key]][
-                        "box"
-                    ] = merge_box_extremes(
-                        [
-                            box
-                            for text, prediction, box in zip(
-                                true_texts[page_indx],
-                                true_predictions[page_indx],
-                                true_boxes[page_indx],
-                            )
-                            if (prediction == value and box != [0, 0, 0, 0])
-                        ]
-                    )
-
-            # #trim empty outputs
-            def item_not_empty(item):
-                return len(item[1]["text"]) != 0
-
-            filtered_output = [
-                dict(filter(item_not_empty, page_output.items()))
-                for page_output in output
-            ]
-
-            if PRINT_DEBUG:
-                print(f"Inference - {time.time() - curr_time:.2} sec")
-            curr_time = time.time()
-
-            # Convert model output to usable format
-            cleaned_output = list(map(clean_model_output, filtered_output))
-            flattened_output = cleaned_output[0] | cleaned_output[1]
-            if not validate_model_output(flattened_output):
-                raise HTTPException(status_code=422, detail="Error validating output")
-
-            if PRINT_DEBUG:
-                print(f"Total time - {time.time() - start_time:.2} sec")
-            curr_time = time.time()
-            return flattened_output
-
-    except Exception:
-        return {"message": "There was an error reading the file"}
-    finally:
+        # Convert model output to usable format
+        cleaned_output = list(map(clean_model_output, filtered_output))
+        flattened_output = cleaned_output[0] | cleaned_output[1]
+        if not validate_model_output(flattened_output):
+            raise HTTPException(status_code=422, detail="Error validating output")
+        
         file.file.close()
+    
+        if PRINT_DEBUG:
+            print(f"DEBUG: Total time - {time.time() - start_time:.2} sec")
+        curr_time = time.time()
+        return flattened_output
+    
